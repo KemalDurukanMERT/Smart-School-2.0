ad271ca154d9fca49fb4b15da86d8b77e489db5a
Serkan Dogan <serkandogan46@gmail.com>
2024-01-19 23:54:46 +0100

    new version added

---
+++ login.py
@@ -0,0 +1,64 @@
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QDialog, QMessageBox
+from PyQt5.QtCore import pyqtSignal
+from user import *
+import hashlib
+
+class LoginApp(QDialog):
+    authentication = pyqtSignal(object)
+    student_registration = pyqtSignal(bool)
+    teacher_registration = pyqtSignal(bool)
+
+    def __init__(self, conn):
+        super(LoginApp, self).__init__()
+        loadUi("login_form.ui", self)
+        self.b1.clicked.connect(self.login)
+        self.b2.clicked.connect(self.show_reg)
+        self.b2_2.clicked.connect(self.show_reg2)
+
+        self.tb2.returnPressed.connect(self.login)
+        self.conn = conn
+
+    def login(self):
+        email = self.tb1.text()
+        password = self.tb2.text()
+
+        cur = self.conn.cursor()
+
+        command = '''
+SELECT * FROM users 
+WHERE email = %s
+'''
+        cur.execute(command, (email,))
+
+        row = cur.fetchone()
+        if row:
+            # Assuming the status is in the 9th column (index 8)
+            status = row[8]
+
+            if status == 'Pending':
+                QMessageBox.warning(self, "Login Error", "Your account is pending approval.")
+                return
+
+            if self.verify_password(password, row[2]):
+                user = User(*row)
+                self.authentication.emit(user)
+            else:
+                QMessageBox.warning(self, "Login Error", "Incorrect Password")    
+        else:
+            QMessageBox.warning(self, "Login Error", "There is no user matched")
+
+        cur.close()
+        self.conn.commit()
+
+    def verify_password(self, entered_password, hashed_password):
+        return self.hash_password(entered_password) == hashed_password
+
+    def hash_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
+    
+    def show_reg(self):
+        self.student_registration.emit(True)
+        
+    def show_reg2(self):
+        self.teacher_registration.emit(True)
---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, user_id, email, hashed_password, name, surname, phone, city, user_type, status, created_time):
+        self.user_id = user_id
+        self.email = email
+        self.password = hashed_password
+        self.name = name
+        self.surname = surname
+        self.user_type = user_type
+        self.status = status
+        self.created_time = created_time
+        self.phone = phone
+        self.city = city
